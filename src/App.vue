e59c02c04ba3a30e4928624dac04a1fa9caef84a
Ana Luczynski <ana.luczynski@salesforce.com>
2018-05-24 12:57:59 -0500

    add sample header for simple navigation

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,35 @@
 <template>
   <div id="app">
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
+    <div class="nav" role="navigation">
+      <router-link :to="{name: 'home'}">Create Requests</router-link>
+      <router-link :to="{name: 'requests'}">View Requests</router-link>
+
+      <span class="faux-company">Welcome back, Brand B!</span>
+    </div>
     <router-view/>
   </div>
 </template>
 
 <style>
+  .nav {
+    background: var(--white1);
+    padding: 20px 10px;
+    display: flex;
+  }
+
+  .nav a,
+  .faux-company {
+    padding: 5px;
+    color: var(--black2);
+  }
+
+  .nav .router-link-exact-active {
+    background: var(--pop1);
+    color: var(--black);
+    text-decoration: none;
+  }
+
+  .faux-company {
+    margin-left: auto;
+  }
 </style>
---
+++ src/views/ViewRequests.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="requests" role="main">
+    <h1>View your requests</h1>
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'viewRequests',
+    components: {
+      
+    }
+  }
+</script>
+
+<style>
+  
+</style>
